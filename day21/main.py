f57dda1d0b30bcbee9b489c84a2d06ab045c1374
Steve Krenzel <steve@krenzel.org>
2021-12-27 04:16:35 -0800

    Day 21 Part 2

--- day21/main.py
+++ day21/main.py
@@ -23,6 +23,7 @@ def take_turn(player_position, die, board_size, rolls):
 
 def play(player_positions, winning_score, board_size=10, rolls=3):
     count, die = create_die(100)
+    player_positions = player_positions[:]
     scores = [0 for _ in player_positions]
     while True:
         for i, position in enumerate(player_positions):
@@ -31,6 +32,48 @@ def play(player_positions, winning_score, board_size=10, rolls=3):
             if scores[i] >= winning_score:
                 return scores, count[0]
 
-print(player_positions,)
+print(player_positions)
 scores, rolls = play(player_positions, 1000)
 print(min(scores) * rolls)
+
+def quantum_play(player_positions, winning_score, player_scores=None, player_turn=0, board_size=10, rolls=3, memoized_results={}):
+    if player_scores is None:
+        player_scores = [0 for _ in player_positions]
+
+    hashable_player_positions = tuple(p for p in player_positions)
+    hashable_player_scores = tuple(s for s in player_scores)
+    memoize_key = (hashable_player_positions, hashable_player_scores, player_turn)
+
+    if memoize_key in memoized_results:
+        return memoized_results[memoize_key]
+
+    results = [0 for _ in player_positions]
+    for player_index in range(len(player_positions)):
+        if player_scores[player_index] >= winning_score:
+            results[player_index] = 1
+            return results
+
+    all_roll_combinations = product(*[list(range(1,4)) for _ in range(rolls)])
+    all_movement_amounts = map(sum, all_roll_combinations)
+    movement_frequencies = Counter(all_movement_amounts)
+
+    player_position = player_positions[player_turn]
+    for move_amount, frequency in movement_frequencies.items():
+        new_position = ((player_position + move_amount - 1) % board_size) + 1
+
+        new_player_positions = player_positions[:]
+        new_player_positions[player_turn] = new_position
+
+        new_player_scores = player_scores[:]
+        new_player_scores[player_turn] += new_position
+
+        next_turn = (player_turn + 1) % len(player_positions)
+        wins = quantum_play(new_player_positions, winning_score, new_player_scores, next_turn, board_size, rolls, memoized_results)
+
+        results = [result + (count * frequency) for result, count in zip(results, wins)]
+
+    memoized_results[memoize_key] = results
+    return results
+
+win_counts = quantum_play(player_positions, 21)
+print(max(win_counts))
